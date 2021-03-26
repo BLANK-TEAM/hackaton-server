2cd8188c8e1efc018bcf2fe9d53bd1a8e8e68a48
Eugene <oleynik.zh10@gmail.com>
2021-03-26 15:54:34 +0200

    cleared

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql'],
-      installSubscriptionHandlers: true,
-    }),],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
