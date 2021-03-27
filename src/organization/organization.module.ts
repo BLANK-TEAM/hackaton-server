a7ce42d68c0b99b5599c1f722dae5486c8183a75
Eugene <oleynik.zh10@gmail.com>
2021-03-27 10:56:06 +0200

    OrgModel fixed

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrganizationModel } from './models/organization.model';
+import { OrganizationResolver } from './organization.resolver';
 import { OrganizationService } from './organization.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: OrganizationModel.name, schema: EventSchema }]),
   ],
-  providers: [OrganizationService],
+  providers: [OrganizationService, OrganizationResolver],
   exports: [OrganizationService]
 })
 export class OrganizationModule { }
